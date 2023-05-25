975c52565c62fcc9f1150f4d21270a68aaa52304
Paz Valenzuela <pazvalenzu@gmail.com>
2023-05-24 20:16:38 -0700

    finalfinal

--- src/js/component/header.jsx
+++ src/js/component/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = (props) => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar navbar-dark bg-dark d-flex">
       <div class="container-fluid fw-semibold">
         <a class="navbar-brand fs-3 " href="#">
           Start Bootstrap</a>
@@ -17,8 +17,8 @@ const Header = (props) => {
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarText">
+          <ul class="navbar-nav ">
             <li class="nav-item fw-semibold">
               <a class="nav-link active " aria-current="page" href="#">
                 Home
